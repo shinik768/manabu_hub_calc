c78748660700cd9f23d58bdc56395a807801171e
shinik768 <139138246+shinik768@users.noreply.github.com>
2024-08-26 09:24:51 +0900

    merged one file

--- app.py
+++ app.py
@@ -7,6 +7,16 @@ import openai
 import os
 import logging
 
+from llama_index import GPTIndex
+from llama_index import Document
+
+# PDFやテキストデータのインデックス化
+documents = [Document.from_file("data/purpose_of_questions_japanese.pdf")]
+index = GPTIndex.from_documents(documents)
+
+# インデックスを保存して再利用
+index.save_to_disk("index.json")
+
 app = Flask(__name__)
 
 port = int(os.environ.get("PORT", 5000))
--- indexer.py
+++
@@ -1,9 +0,0 @@
-from llama_index import GPTIndex
-from llama_index import Document
-
-# PDFやテキストデータのインデックス化
-documents = [Document.from_file("data/purpose_of_questions_japanese.pdf")]
-index = GPTIndex.from_documents(documents)
-
-# インデックスを保存して再利用
-index.save_to_disk("index.json")
