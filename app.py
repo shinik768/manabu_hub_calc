3de2c587fc394c865e81d813270264bd86ef19a4
shinik768 <139138246+shinik768@users.noreply.github.com>
2024-08-26 12:49:13 +0900

    add apikeys

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
-
+#from llama_index import GPTIndex  # または適切なモジュール名
 from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+import openai
 import os
 import logging
 
@@ -13,6 +17,21 @@ logging.basicConfig(level=logging.ERROR)
 @app.route('/')
 def home():
     return "<p>Hello, World!</p>"
+
+line_bot_api = LineBotApi(
+    'd580vu4FvFiDqyU6Qxi3xhbx8d24lOFcGDKREcASB3QQdIjhq2+wq22Dcml1RMHf0xGd8fj3LGbkQvteTwr3EV+x4kuba/boP+YTFrS3KQt5hIzSY96Tqu0khsEbCKgelGZDv/KRlDmjeEVHEoqYHQdB04t89/1O/w1cDnyilFU='
+)
+handler = WebhookHandler('ede248563361ab924f8f81e7c425038b')
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return 'OK'
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=port)
