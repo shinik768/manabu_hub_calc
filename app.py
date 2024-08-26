0801acd9a77ba2ea6c0578765b25644d07c62c32
shinik768 <139138246+shinik768@users.noreply.github.com>
2024-08-26 13:36:09 +0900

    a

--- app.py
+++ app.py
@@ -1,53 +1,34 @@
 import os
 from flask import Flask, abort, request
-from linebot.v3.webhook import (
-    WebhookHandler
-)
-from linebot.v3.exceptions import (
-    InvalidSignatureError
-)
-from linebot.v3.messaging import (
-    Configuration,
-    ApiClient,
-    MessagingApi,
-    ReplyMessageRequest,
-    TextMessage
-)
-from linebot.v3.webhooks import (
-    MessageEvent,
-    TextMessageContent
-)
-
+from linebot.v3.webhook import WebhookHandler
+from linebot.v3.exceptions import InvalidSignatureError
+from linebot.v3.messaging import Configuration, ApiClient, MessagingApi, ReplyMessageRequest, TextMessage
+from linebot.v3.webhooks import MessageEvent, TextMessageContent
 
 app = Flask(__name__)
 
-handler = WebhookHandler('ede248563361ab924f8f81e7c425038b')
-configuration = Configuration(access_token='d580vu4FvFiDqyU6Qxi3xhbx8d24lOFcGDKREcASB3QQdIjhq2+wq22Dcml1RMHf0xGd8fj3LGbkQvteTwr3EV+x4kuba/boP+YTFrS3KQt5hIzSY96Tqu0khsEbCKgelGZDv/KRlDmjeEVHEoqYHQdB04t89/1O/w1cDnyilFU=')
+# 環境変数からアクセストークンとシークレットを取得
+handler = WebhookHandler(os.environ['LINE_CHANNEL_SECRET'])
+configuration = Configuration(access_token=os.environ['LINE_CHANNEL_ACCESS_TOKEN'])
 
-"""
 @app.route("/callback", methods=['POST'])
 def callback():
-    # get X-Line-Signature header value
+    # X-Line-Signature ヘッダー値を取得
     signature = request.headers['X-Line-Signature']
-
-    # get request body as text
     body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
 
-    # handle webhook body
+    # Webhookボディを処理
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
-        app.logger.info("Invalid signature. Please check your channel access token/channel secret.")
         abort(400)
 
     return 'OK'
 
-
 @handler.add(MessageEvent, message=TextMessageContent)
 def handle_message(event):
     with ApiClient(configuration) as api_client:
-        #相手の送信した内容で条件分岐して回答を変数に代入
+        # ユーザーのメッセージに応じたレスポンス
         if event.message.text == 'グー':
             msg = 'パー'
         elif event.message.text == 'チョキ':
@@ -64,9 +45,7 @@ def handle_message(event):
                 messages=[TextMessage(text=msg)]
             )
         )
-"""
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=False)
-
