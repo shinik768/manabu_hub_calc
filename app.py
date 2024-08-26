b909dfe5ea8b7b6954b79a3f4763bacabeae8dc0
shinik768 <139138246+shinik768@users.noreply.github.com>
2024-08-26 13:46:48 +0900

    a

--- app.py
+++ app.py
@@ -1,51 +1,52 @@
 import os
-from flask import Flask, abort, request
-from linebot.v3.webhook import WebhookHandler
-from linebot.v3.exceptions import InvalidSignatureError
-from linebot.v3.messaging import Configuration, ApiClient, MessagingApi, ReplyMessageRequest, TextMessage
-from linebot.v3.webhooks import MessageEvent, TextMessageContent
+import logging
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from dotenv import load_dotenv
+
+# 環境変数のロード
+load_dotenv()
 
 app = Flask(__name__)
 
-# 環境変数からアクセストークンとシークレットを取得
-handler = WebhookHandler(os.environ['LINE_CHANNEL_SECRET'])
-configuration = Configuration(access_token=os.environ['LINE_CHANNEL_ACCESS_TOKEN'])
+port = int(os.environ.get("PORT", 5000))
+
+# ロギングの設定
+logging.basicConfig(level=logging.ERROR)
+
+# 環境変数からLINE APIの設定を取得
+line_bot_api = LineBotApi(os.getenv('LINE_CHANNEL_ACCESS_TOKEN'))
+handler = WebhookHandler(os.getenv('LINE_CHANNEL_SECRET'))
+
+@app.route('/')
+def home():
+    return "<p>Hello, World!</p>"
 
 @app.route("/callback", methods=['POST'])
 def callback():
-    # X-Line-Signature ヘッダー値を取得
     signature = request.headers['X-Line-Signature']
     body = request.get_data(as_text=True)
-
-    # Webhookボディを処理
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
-
     return 'OK'
 
-@handler.add(MessageEvent, message=TextMessageContent)
+@handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    with ApiClient(configuration) as api_client:
-        # ユーザーのメッセージに応じたレスポンス
-        if event.message.text == 'グー':
-            msg = 'パー'
-        elif event.message.text == 'チョキ':
-            msg = 'グー'
-        elif event.message.text == 'パー':
-            msg = 'チョキ'
-        else:
-            msg = 'ごめんね。\nまだ他のメッセージには対応してないよ'
+    # LINEに応答を送信
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text="Hello, World!")  # ここで応答を設定
+    )
 
-        line_bot_api = MessagingApi(api_client)
-        line_bot_api.reply_message_with_http_info(
-            ReplyMessageRequest(
-                reply_token=event.reply_token,
-                messages=[TextMessage(text=msg)]
-            )
-        )
+@app.errorhandler(Exception)
+def handle_exception(e):
+    # エラーメッセージをログに記録
+    logging.error(f"An error occurred: {str(e)}")
+    return str(e), 500
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=False)
+    app.run(host='0.0.0.0', port=port)
