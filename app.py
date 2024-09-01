eaba2ab4d5dd71e8a0b866828f799c51cb8a5c69
shinik768 <139138246+shinik768@users.noreply.github.com>
2024-09-02 00:11:26 +0900

    print results

--- app.py
+++ app.py
@@ -65,10 +65,12 @@ def handle_message(event):
             
             # LINE APIクライアントの作成
             line_bot_api = MessagingApi(ApiClient(configuration))
+            print(results_str)
 
             results_str = split_message(result_str, max_length=5000)
             messages = [ImageMessage(original_content_url=image_url, preview_image_url=image_url)]
             messages.extend([TextMessage(text=result_str) for result_str in results_str])
+            print(messages)
             
             # 画像メッセージとテキストメッセージを同時に送信
             line_bot_api.reply_message_with_http_info(
